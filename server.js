c456756eab9f932d34de4fa8e8a46869a1a897cc
HomamZituni <homam93@msn.com>
2026-01-05 21:03:48 -0500

    vercel update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html')); });
