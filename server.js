f605ab575a8d36beace050bf6134704996d1c56d
HomamZituni <homam93@msn.com>
2026-01-05 20:55:35 -0500

    completed troubleshooting of express server set up

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html')); });
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/contact.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
