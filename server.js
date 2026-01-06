a566cb633e7a23a498af235e70408f1eb8d65fdc
HomamZituni <homam93@msn.com>
2026-01-05 21:07:11 -0500

    updated for vercel

--- server.js
+++ server.js
@@ -6,15 +6,20 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html')); });
+    res.sendFile(path.join(__dirname, 'public/index.html')); 
+});
 
 app.get('/contact', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/contact.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+export default app;
+
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
